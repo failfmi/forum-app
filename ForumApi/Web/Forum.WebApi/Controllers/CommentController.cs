65aab7919f56890f3bf07a22b1a880bd37192841
failfmi <fifty_cen7@abv.bg>
2018-12-25 12:59:20 +0200

    update comment controller -> add missing endpoints

--- ForumApi/Web/Forum.WebApi/Controllers/CommentController.cs
+++ ForumApi/Web/Forum.WebApi/Controllers/CommentController.cs
@@ -44,6 +44,72 @@ namespace Forum.WebApi.Controllers
                 return this.BadRequest(new ReturnMessage {Message = e.Message});
             }
         }
-        
+
+        [Authorize]
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<object> Edit(int id, [FromBody] CommentInputEditModel model)
+        {
+            if (id != model.Id)
+            {
+                return this.BadRequest(new ReturnMessage { Message = "Invalid ids" });
+            }
+
+            try
+            {
+                var comment = await this.commentService.Edit(model, this.User.FindFirst(ClaimTypes.Name).Value);
+                return this.Ok(new CreateEditReturnMessage<CommentViewModel>
+                    { Message = "Comment edited successfully", Data = comment });
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                return this.Unauthorized(new ReturnMessage { Message = e.Message });
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new ReturnMessage { Message = e.Message });
+            }
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                await this.commentService.Delete(id, this.User.FindFirst(ClaimTypes.Name).Value);
+                return this.Ok(new ReturnMessage { Message = "Comment deleted successfully!" });
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                return this.Unauthorized(new ReturnMessage { Message = e.Message });
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new ReturnMessage { Message = e.Message });
+            }
+        }
+
+
+        [HttpGet("{username}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesDefaultResponseType]
+        public async Task<object> Get(string username)
+        {
+            try
+            {
+                return this.Ok(this.commentService.GetCommentsByUsername(username));
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new ReturnMessage {Message = e.Message});
+            }
+        }
     }
 }
